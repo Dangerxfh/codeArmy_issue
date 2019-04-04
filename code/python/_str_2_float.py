a294fc20a1cf20dd8666906fcd61ef5cf8609186
xufuhao <fuhao.xu@chelaile.net.cn>
2019-04-04 17:37:56 +0800

    str2float.py

---
+++ code/python/_str_2_float.py
@@ -0,0 +1,8 @@
+#思路：比如123.999   根据"."分割成，转成数字后再123+999/10^3=123+0.999=123.999
+from functools import reduce
+def _str_2_float(s):
+    return  reduce(lambda x,y: x+y/(10**len(str(y))),map(int,s.split(".")))
+print(_str_2_float("123.999")+0.001)
+
+#输出
+# 124.0
